7344aafb41c7977dac89a32c9c9611c1282abc90
Divy Patel <divy9881@gmail.com>
2023-10-07 13:23:13 -0500

    fix: indentation
    Signed-off-by: Divy Patel <divy9881@gmail.com>

--- External-Sort/DataRecord.cpp
+++ External-Sort/DataRecord.cpp
@@ -5,7 +5,7 @@ DataRecord::DataRecord (int col1, int col2, int col3)
     this->_record[0] = col1;
     this->_record[1] = col2;
     this->_record[2] = col3;
-	TRACE (true);
+    TRACE (true);
 } // DataRecord::DataRecord (int col1, int col2, int col3)
 
 DataRecord::DataRecord (const DataRecord& record)
@@ -21,11 +21,11 @@ DataRecord::~DataRecord ()
     this->_record[0] = INT_MIN;
     this->_record[1] = INT_MIN;
     this->_record[2] = INT_MIN;
-	TRACE (true);
+    TRACE (true);
 } // DataRecord::~DataRecord
 
 void DataRecord::print ()
 {
     printf("%d %d %d\n", this->_record[0], this->_record[1], this->_record[2]);
-	TRACE (true);
+    TRACE (true);
 } // DataRecord::~DataRecord
