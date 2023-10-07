9ddd42df2d135282efea201a104a8325fa1a1144
Divy Patel <divy9881@gmail.com>
2023-10-07 13:20:23 -0500

    fix: indentation
    Signed-off-by: Divy Patel <divy9881@gmail.com>

--- External-Sort/DataRecord.h
+++ External-Sort/DataRecord.h
@@ -5,11 +5,11 @@
 class DataRecord
 {
 public:
-	DataRecord (int col1, int col2, int col3);
+    DataRecord (int col1, int col2, int col3);
     DataRecord (const DataRecord& record);
-	~DataRecord ();
+    ~DataRecord ();
     void print();
 
 private:
-	int _record[3];
+    int _record[3];
 }; // class DataRecord
--- External-Sort/Scan.cpp
+++ External-Sort/Scan.cpp
@@ -2,46 +2,46 @@
 
 ScanPlan::ScanPlan (RowCount const count) : _count (count)
 {
-	this->_rows = (DataRecord *)malloc(this->_count * sizeof(DataRecord));
-	TRACE (true);
+    this->_rows = (DataRecord *)malloc(this->_count * sizeof(DataRecord));
+    TRACE (true);
 } // ScanPlan::ScanPlan
 
 ScanPlan::~ScanPlan ()
 {
-	free(this->_rows);
-	TRACE (true);
+    free(this->_rows);
+    TRACE (true);
 } // ScanPlan::~ScanPlan
 
 Iterator * ScanPlan::init () const
 {
-	TRACE (true);
-	return new ScanIterator (this);
+    TRACE (true);
+    return new ScanIterator (this);
 } // ScanPlan::init
 
 ScanIterator::ScanIterator (ScanPlan const * const plan) :
 	_plan (plan), _count (0)
 {
-	TRACE (true);
+    TRACE (true);
 } // ScanIterator::ScanIterator
 
 ScanIterator::~ScanIterator ()
 {
-	TRACE (true);
-	traceprintf ("produced %lu of %lu rows\n",
-			(unsigned long) (_count),
-			(unsigned long) (_plan->_count));
+    TRACE (true);
+    traceprintf ("produced %lu of %lu rows\n",
+            (unsigned long) (_count),
+            (unsigned long) (_plan->_count));
 } // ScanIterator::~ScanIterator
 
 bool ScanIterator::next ()
 {
-	TRACE (true);
+    TRACE (true);
 
-	if (_count >= _plan->_count)
-		return false;
+    if (_count >= _plan->_count)
+        return false;
 
-	DataRecord row = DataRecord(Random(RANDOM_INTEGER_RANGE), Random(RANDOM_INTEGER_RANGE), Random(RANDOM_INTEGER_RANGE));
-	this->_plan->_rows[this->_count] = row;
-	row.print();
-	++ _count;
-	return true;
+    DataRecord row = DataRecord(Random(RANDOM_INTEGER_RANGE), Random(RANDOM_INTEGER_RANGE), Random(RANDOM_INTEGER_RANGE));
+    this->_plan->_rows[this->_count] = row;
+    row.print();
+    ++ _count;
+    return true;
 } // ScanIterator::next
