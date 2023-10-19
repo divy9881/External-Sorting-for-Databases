586fb7f2ca1df598b6b4fca366877c98604e6d5e
Devaki Kulkarni <disk1269@gmail.com>
2023-10-19 14:55:49 +0000

    Restructured class files

---
+++ External-Sort/DataRecord.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "defs.h"
+
+class DataRecord
+{
+public:
+    DataRecord (int col1, int col2, int col3);
+    DataRecord (const DataRecord& record);
+    DataRecord ();
+    ~DataRecord ();
+    void print();
+    bool operator<(const DataRecord& other) const;
+    bool operator==(const DataRecord& other) const;
+
+private:
+    int _record[3];
+}; // class DataRecord
