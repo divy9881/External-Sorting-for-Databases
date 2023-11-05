da4faaa7ddbb1c769b17b2a4c24e6f7340c1020b
Divy Patel <divy9881@gmail.com>
2023-11-05 17:34:14 -0600

    fix: change space-based indentation to tab-based
    Signed-off-by: Divy Patel <divy9881@gmail.com>

--- External-Sort/Scan.cpp
+++ External-Sort/Scan.cpp
@@ -2,51 +2,51 @@
 
 ScanPlan::ScanPlan (RowCount const count) : _count (count)
 {
-    this->_rows = (DataRecord *)malloc(this->_count * sizeof(DataRecord));
-    for (RowCount ii = 0; ii < _count; ii++) {
-        this->_rows[ii].SetRecord(Random(RANDOM_INTEGER_RANGE), Random(RANDOM_INTEGER_RANGE), Random(RANDOM_INTEGER_RANGE));
-    }
-    TRACE (true);
+	this->_rows = (DataRecord *)malloc(this->_count * sizeof(DataRecord));
+	for (RowCount ii = 0; ii < _count; ii++) {
+		this->_rows[ii].SetRecord(Random(RANDOM_INTEGER_RANGE), Random(RANDOM_INTEGER_RANGE), Random(RANDOM_INTEGER_RANGE));
+	}
+	TRACE (true);
 } // ScanPlan::ScanPlan
 
 ScanPlan::~ScanPlan ()
 {
-    free(this->_rows);
-    TRACE (true);
+	free(this->_rows);
+	TRACE (true);
 } // ScanPlan::~ScanPlan
 
 Iterator * ScanPlan::init () const
 {
-    TRACE (true);
-    return new ScanIterator (this);
+	TRACE (true);
+	return new ScanIterator (this);
 } // ScanPlan::init
 
 DataRecord ScanPlan::GetRecord(RowCount const rowid) const
 {
-    return this->_rows[rowid];
+	return this->_rows[rowid];
 } // ScanPlan::GetRecord
 
 ScanIterator::ScanIterator (ScanPlan const * const plan) :
 	_plan (plan), _count (0)
 {
-    TRACE (true);
+	TRACE (true);
 } // ScanIterator::ScanIterator
 
 ScanIterator::~ScanIterator ()
 {
-    TRACE (true);
-    traceprintf ("produced %lu of %lu rows\n",
-            (unsigned long) (_count),
-            (unsigned long) (_plan->_count));
+	TRACE (true);
+	traceprintf ("produced %lu of %lu rows\n",
+			(unsigned long) (_count),
+			(unsigned long) (_plan->_count));
 } // ScanIterator::~ScanIterator
 
 bool ScanIterator::next ()
 {
-    TRACE (true);
+	TRACE (true);
 
-    if (_count >= _plan->_count)
-        return false;
-    _plan->GetRecord(_count).print();
-    ++ _count;
-    return true;
+	if (_count >= _plan->_count)
+		return false;
+	_plan->GetRecord(_count).print();
+	++ _count;
+	return true;
 } // ScanIterator::next
--- External-Sort/Tree.cpp
+++ External-Sort/Tree.cpp
@@ -10,20 +10,20 @@
 #define NODE_RECORD_LIST_LENGTH(node) node->list->record_count
 
 DataRecord* pop_record(RecordList *list) {
-    DataRecord* top = NULL;
-    if (list->record_count == 1) {
-        top = &(list->data[0]);
-        list->data = NULL;
-    } else if (list->record_count > 1) {
-        top = &(list->data[0]);
-        list->data = &(list->data[1]);
-    }
-    list->record_count--;
-    return top;
+	DataRecord* top = NULL;
+	if (list->record_count == 1) {
+		top = &(list->data[0]);
+		list->data = NULL;
+	} else if (list->record_count > 1) {
+		top = &(list->data[0]);
+		list->data = &(list->data[1]);
+	}
+	list->record_count--;
+	return top;
 }
 
 DataRecord* top_record(RecordList *list) {
-    return list->data;
+	return list->data;
 }
 
 /**
@@ -40,220 +40,220 @@ DataRecord* top_record(RecordList *list) {
 */
 Tree::Tree(DataRecord *records, int record_ct, int initial_run)
 {
-    // TODO See if this is optimal division for fanning
-    this->total_leaves = ceil(log2(record_ct));
-    DataRecord *current_ptr = records;
-    int count_of_cols_per_row = ceil(record_ct/this->total_leaves);
+	// TODO See if this is optimal division for fanning
+	this->total_leaves = ceil(log2(record_ct));
+	DataRecord *current_ptr = records;
+	int count_of_cols_per_row = ceil(record_ct/this->total_leaves);
 
-    if (initial_run) {
-        count_of_cols_per_row = 1;
-        this->total_leaves = record_ct;
-    }
+	if (initial_run) {
+		count_of_cols_per_row = 1;
+		this->total_leaves = record_ct;
+	}
 
-    this->tree_depth = ceil(log2(this->total_leaves));
-    this->total_nodes = 2 * pow(2, this->tree_depth) - 1;
-    this->total_record_count = record_ct;
-    this->heap = std::vector <struct Node>(this->total_nodes);
+	this->tree_depth = ceil(log2(this->total_leaves));
+	this->total_nodes = 2 * pow(2, this->tree_depth) - 1;
+	this->total_record_count = record_ct;
+	this->heap = vector <struct Node>(this->total_nodes);
 
-    int first_leaf_node = this->total_nodes - ((this->total_nodes - 1)/2) - 1;
+	int first_leaf_node = this->total_nodes - ((this->total_nodes - 1)/2) - 1;
 
-    int current_ct = record_ct;
+	int current_ct = record_ct;
 
-    // We always try to generate full binary tree at the beginning
-    // (last leaf may not be balanced)
-    for (lluint ii = first_leaf_node ; ii < (this->total_leaves*2) - 1 ; ii++) {
-        // Leaf nodes has no current record
-        this->heap[ii].current_record = NULL;
-        this->heap[ii].is_empty = false;
-        this->heap[ii].is_leaf = true;
+	// We always try to generate full binary tree at the beginning
+	// (last leaf may not be balanced)
+	for (lluint ii = first_leaf_node ; ii < (this->total_leaves*2) - 1 ; ii++) {
+		// Leaf nodes has no current record
+		this->heap[ii].current_record = NULL;
+		this->heap[ii].is_empty = false;
+		this->heap[ii].is_leaf = true;
 
-        // Assign records to each row
-        this->heap[ii].list = (RecordList *) malloc(sizeof(RecordList));
-        this->heap[ii].list->data = (DataRecord *) malloc(sizeof(DataRecord*));
-        this->heap[ii].list->data = current_ptr;
-        this->heap[ii].list->record_count = count_of_cols_per_row;
+		// Assign records to each row
+		this->heap[ii].list = (RecordList *) malloc(sizeof(RecordList));
+		this->heap[ii].list->data = (DataRecord *) malloc(sizeof(DataRecord*));
+		this->heap[ii].list->data = current_ptr;
+		this->heap[ii].list->record_count = count_of_cols_per_row;
 
-        if (current_ct > 0) {
-            current_ptr = current_ptr + count_of_cols_per_row;
-            current_ct -= count_of_cols_per_row;
-        } else {
-            break;
-        }
-        // Sample calculation:
-        // For 128 records, there will be 7 rows == 
-        //          18 count_of_cols_per_row + 2 remaining
-        // for last row, current_ct will be 2 after subtraction,
-        // so we will just add it to the last row
-        if (initial_run == 0) {
-            if ((current_ct > 0) && (current_ct <= 2*count_of_cols_per_row)) {
-                count_of_cols_per_row = current_ct;
-                current_ct = -1;
-            }
-        }
-    }
+		if (current_ct > 0) {
+			current_ptr = current_ptr + count_of_cols_per_row;
+			current_ct -= count_of_cols_per_row;
+		} else {
+			break;
+		}
+		// Sample calculation:
+		// For 128 records, there will be 7 rows == 
+		//          18 count_of_cols_per_row + 2 remaining
+		// for last row, current_ct will be 2 after subtraction,
+		// so we will just add it to the last row
+		if (initial_run == 0) {
+			if ((current_ct > 0) && (current_ct <= 2*count_of_cols_per_row)) {
+				count_of_cols_per_row = current_ct;
+				current_ct = -1;
+			}
+		}
+	}
 }
 
 int Tree::capacity(int level) {
-    return (1<<level);
+	return (1<<level);
 }
 
 /*
- * Compares the record in children nodes in the tree, and pulls up the winner record.
- * Runs only for the internal nodes.
- * @param parent Index (in heap) of the parent record
- */
+* Compares the record in children nodes in the tree, and pulls up the winner record.
+* Runs only for the internal nodes.
+* @param parent Index (in heap) of the parent record
+*/
 void Tree::compare_and_swap(int parent, int unused_leaves_idx) {
-    int child_left = parent*2+1, child_right=parent*2+2;
-    struct Node *parent_node = &this->heap[parent];
+	int child_left = parent*2+1, child_right=parent*2+2;
+	struct Node *parent_node = &this->heap[parent];
 
-    // None of the children are valid == not being used as runs
-    if (child_left >= unused_leaves_idx) {
-        return;
-    } else if (child_left < unused_leaves_idx /*Only left child is valid, special case only valid at leaf nodes */ 
-                && child_right >= unused_leaves_idx) {
-            struct Node *left_child_node = &this->heap[child_left];
-            // If parent has a valid record, it will be the winner, so skip.
-            // Iteration over the next level will empty this node
-            // the iteration after that for this level would fill it up.
-            if (parent_node->current_record) {
-                return;
-            } else {
-                if (IS_LEAF_NODE(left_child_node)) {
-                    parent_node->current_record = pop_record(left_child_node->list);
-                }
-            }
-    } else { /* Both the children are valid. So add the popped record at parent (if empty) */
-        if (parent_node->current_record) {
-            return;
-        } else {
-            struct Node *left_child_node = &this->heap[child_left];
-            struct Node *right_child_node = &this->heap[child_right];
-            DataRecord *left_data = NULL, *right_data = NULL;
-            if (IS_LEAF_NODE(left_child_node)) {
-                // Both will be a leaf node
-                left_data = top_record(left_child_node->list);
-                right_data = top_record(right_child_node->list);
-                if (left_data && right_data) {
-                    // Compare
-                    if (left_data->_record[0] <= right_data->_record[0]) {
-                        parent_node->current_record = pop_record(left_child_node->list);
-                    } else {
-                        parent_node->current_record = pop_record(right_child_node->list);
-                    }
-                } else if (left_data) {
-                    parent_node->current_record = pop_record(left_child_node->list);
-                } else if (right_data) {
-                    parent_node->current_record = pop_record(right_child_node->list);
-                } else {
-                    parent_node->current_record = NULL;
-                }
-            } else {
-                // It is an internal node
-                left_data = NODE_CURRENT_RECORD(left_child_node);
-                right_data = NODE_CURRENT_RECORD(right_child_node);
-                if (left_data && right_data) {
-                    // Compare
-                    if (left_data->_record[0] <= right_data->_record[0]) {
-                        parent_node->current_record = left_data;
-                        NODE_CURRENT_RECORD(left_child_node) = NULL;
-                    } else {
-                        parent_node->current_record = right_data;
-                        NODE_CURRENT_RECORD(right_child_node) = NULL;
-                    }
-                } else if (left_data) {
-                    parent_node->current_record = left_data;
-                    NODE_CURRENT_RECORD(left_child_node) = NULL;
-                } else if (right_data) {
-                    parent_node->current_record = right_data;
-                    NODE_CURRENT_RECORD(right_child_node) = NULL;
-                } else {
-                    parent_node->current_record = NULL;
-                }
-            }
-        }
-    }
+	// None of the children are valid == not being used as runs
+	if (child_left >= unused_leaves_idx) {
+		return;
+	} else if (child_left < unused_leaves_idx /*Only left child is valid, special case only valid at leaf nodes */ 
+				&& child_right >= unused_leaves_idx) {
+			struct Node *left_child_node = &this->heap[child_left];
+			// If parent has a valid record, it will be the winner, so skip.
+			// Iteration over the next level will empty this node
+			// the iteration after that for this level would fill it up.
+			if (parent_node->current_record) {
+				return;
+			} else {
+				if (IS_LEAF_NODE(left_child_node)) {
+					parent_node->current_record = pop_record(left_child_node->list);
+				}
+			}
+	} else { /* Both the children are valid. So add the popped record at parent (if empty) */
+		if (parent_node->current_record) {
+			return;
+		} else {
+			struct Node *left_child_node = &this->heap[child_left];
+			struct Node *right_child_node = &this->heap[child_right];
+			DataRecord *left_data = NULL, *right_data = NULL;
+			if (IS_LEAF_NODE(left_child_node)) {
+				// Both will be a leaf node
+				left_data = top_record(left_child_node->list);
+				right_data = top_record(right_child_node->list);
+				if (left_data && right_data) {
+					// Compare
+					if (left_data->_record[0] <= right_data->_record[0]) {
+						parent_node->current_record = pop_record(left_child_node->list);
+					} else {
+						parent_node->current_record = pop_record(right_child_node->list);
+					}
+				} else if (left_data) {
+					parent_node->current_record = pop_record(left_child_node->list);
+				} else if (right_data) {
+					parent_node->current_record = pop_record(right_child_node->list);
+				} else {
+					parent_node->current_record = NULL;
+				}
+			} else {
+				// It is an internal node
+				left_data = NODE_CURRENT_RECORD(left_child_node);
+				right_data = NODE_CURRENT_RECORD(right_child_node);
+				if (left_data && right_data) {
+					// Compare
+					if (left_data->_record[0] <= right_data->_record[0]) {
+						parent_node->current_record = left_data;
+						NODE_CURRENT_RECORD(left_child_node) = NULL;
+					} else {
+						parent_node->current_record = right_data;
+						NODE_CURRENT_RECORD(right_child_node) = NULL;
+					}
+				} else if (left_data) {
+					parent_node->current_record = left_data;
+					NODE_CURRENT_RECORD(left_child_node) = NULL;
+				} else if (right_data) {
+					parent_node->current_record = right_data;
+					NODE_CURRENT_RECORD(right_child_node) = NULL;
+				} else {
+					parent_node->current_record = NULL;
+				}
+			}
+		}
+	}
 }
 
 struct Node Tree::leaf(int index, int current_slot) {
-    return this->heap[current_slot*2 + index];
+	return this->heap[current_slot*2 + index];
 }
 
 struct Node Tree::parent(int current_slot) {
-    return this->heap[current_slot/2];
+	return this->heap[current_slot/2];
 }
 
 /*
- * Each call runs the tree completely to generate a sorted run of the passed values
- * (for all values in the run).
- */
+* Each call runs the tree completely to generate a sorted run of the passed values
+* (for all values in the run).
+*/
 void Tree::run_tree() {
-    int unused_leaves_idx = (this->total_nodes + 1) / 2 - 1 + this->total_leaves;
+	int unused_leaves_idx = (this->total_nodes + 1) / 2 - 1 + this->total_leaves;
 
-    // Run tree will complete one merge for all records. If we want a partial one, should update this.
-    // Run tree will complete one merge for all records. If we want a partial one, should update this.
-    for (lluint iteration = 0 ; iteration < this->total_record_count; iteration++) {
-        // Each iteration will give one of the priority queue elements,
-        // run for each of the inner nodes
-        for (int inner_node_idx = this->total_nodes - pow(2, this->tree_depth) - 1;
-                inner_node_idx >= 0;
-                inner_node_idx--) {
-            this->compare_and_swap(inner_node_idx, unused_leaves_idx);
-        }
-        // printf("\nAfter iteration %d ", iteration); this->print_heap();
-        // printf("Winner is :: "); 
-        // this->heap[0].current_record->print(); printf("\n");
-        this->generated_run.push_back(this->heap[0].current_record);
-        this->heap[0].current_record = NULL;
-    }
+	// Run tree will complete one merge for all records. If we want a partial one, should update this.
+	// Run tree will complete one merge for all records. If we want a partial one, should update this.
+	for (lluint iteration = 0 ; iteration < this->total_record_count; iteration++) {
+		// Each iteration will give one of the priority queue elements,
+		// run for each of the inner nodes
+		for (int inner_node_idx = this->total_nodes - pow(2, this->tree_depth) - 1;
+				inner_node_idx >= 0;
+				inner_node_idx--) {
+			this->compare_and_swap(inner_node_idx, unused_leaves_idx);
+		}
+		// printf("\nAfter iteration %d ", iteration); this->print_heap();
+		// printf("Winner is :: "); 
+		// this->heap[0].current_record->print(); printf("\n");
+		this->generated_run.push_back(this->heap[0].current_record);
+		this->heap[0].current_record = NULL;
+	}
 }
 
 /*
- * Prints (index Empty) for empty nodes
- * Prints "count -> [heap_index @ list_index :: (datarecord)]" list
- * 
- * For e.g.:
- * (1 Empty )
- * (2 Empty )
- * Count: 0 ->
- * Count: 1 -> [5 @ 0 :: (6, 6, 6)]
- * Count: 3 -> [6 @ 0 :: (7, 7, 7)] [6 @ 1 :: (8, 8, 8)] [6 @ 2 :: (9, 9, 9)]
- */
+* Prints (index Empty) for empty nodes
+* Prints "count -> [heap_index @ list_index :: (datarecord)]" list
+* 
+* For e.g.:
+* (1 Empty )
+* (2 Empty )
+* Count: 0 ->
+* Count: 1 -> [5 @ 0 :: (6, 6, 6)]
+* Count: 3 -> [6 @ 0 :: (7, 7, 7)] [6 @ 1 :: (8, 8, 8)] [6 @ 2 :: (9, 9, 9)]
+*/
 void Tree::print_heap() {
-    for (lluint ii = 0 ; ii < this->total_nodes; ii++) {
-        if (!this->heap[ii].is_empty) {
-            if (this->heap[ii].current_record) {
-                printf("%lld :: (%d, %d, %d)\n",
-                        ii, this->heap[ii].current_record->_record[0],
-                        this->heap[ii].current_record->_record[1],
-                        this->heap[ii].current_record->_record[2]);
-            } else {
-                RecordList *heap_list = this->heap[ii].list;
-                DataRecord *current_record = heap_list->data;
+	for (lluint ii = 0 ; ii < this->total_nodes; ii++) {
+		if (!this->heap[ii].is_empty) {
+			if (this->heap[ii].current_record) {
+				printf("%lld :: (%d, %d, %d)\n",
+						ii, this->heap[ii].current_record->_record[0],
+						this->heap[ii].current_record->_record[1],
+						this->heap[ii].current_record->_record[2]);
+			} else {
+				RecordList *heap_list = this->heap[ii].list;
+				DataRecord *current_record = heap_list->data;
 
-                printf("Count: %lld -> ", heap_list->record_count);
-                for (lluint jj = 0; jj < this->heap[ii].list->record_count; jj++) {
-                    printf("[%lld @ %lld :: (%d, %d, %d)] ",
-                        ii, jj, current_record->_record[0],
-                        current_record->_record[1],
-                        current_record->_record[0]);
-                    current_record++;
-                }
-                printf("\n");
-            }
-        } else {
-            printf("\n(%lld Empty )\n", ii);
-        }
-    }
+				printf("Count: %lld -> ", heap_list->record_count);
+				for (lluint jj = 0; jj < this->heap[ii].list->record_count; jj++) {
+					printf("[%lld @ %lld :: (%d, %d, %d)] ",
+						ii, jj, current_record->_record[0],
+						current_record->_record[1],
+						current_record->_record[0]);
+					current_record++;
+				}
+				printf("\n");
+			}
+		} else {
+			printf("\n(%lld Empty )\n", ii);
+		}
+	}
 }
 
 /*
- * Prints a sorted run
- */
+* Prints a sorted run
+*/
 void Tree::print_run() {
-    for (auto a: this->generated_run) {
-        a->print();
-    }
-    return;
+	for (auto a: this->generated_run) {
+		a->print();
+	}
+	return;
 }
 
 Tree::~Tree ()
