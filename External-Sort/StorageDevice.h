0b13b0c90d4b62d8e8d64ca4060cc30e01a3126d
Divy Patel <divy9881@gmail.com>
2023-11-14 15:25:05 -0600

    fix: minor changes
    Signed-off-by: Divy Patel <divy9881@gmail.com>

--- External-Sort/StorageDevice.h
+++ External-Sort/StorageDevice.h
@@ -6,20 +6,20 @@
 
 class StorageDevice
 {
-    public:
-        StorageDevice(string, lluint, uint);
-        pair<DataRecord *, uint> get_run_page(uint);
-        void spill_run(DataRecord *, uint);
-        void truncate_device();
-    protected:
-        uint num_runs;
-        uint max_runs;
-        string device_path;
-        lluint free_space;
-        lluint total_space;
-        uint *run_offsets;
+	public:
+		StorageDevice(string, lluint, uint);
+		pair<DataRecord *, uint> get_run_page(uint);
+		void spill_run(DataRecord *, uint);
+		void truncate_device();
+	protected:
+		uint num_runs;
+		uint max_runs;
+		string device_path;
+		lluint free_space;
+		lluint total_space;
+		uint *run_offsets;
 
-        void spill_run_to_disk(string, DataRecord *, uint);
-        pair<DataRecord *, uint> get_run_page_from_disk(string, uint *, uint);
-        int truncate_all_runs();
+		void spill_run_to_disk(string, DataRecord *, uint);
+		pair<DataRecord *, uint> get_run_page_from_disk(string, uint *, uint);
+		int truncate_all_runs();
 };
